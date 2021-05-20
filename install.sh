630fb29b91a8da38c9be3ac6d57feb6b0e31a131
Arthur Huynh <9056483+ddh@users.noreply.github.com>
2021-05-20 07:52:48 -0700

    Check for Big Sur

--- install.sh
+++ install.sh
@@ -76,8 +76,13 @@
 
       MAC_OS_VERSION="$(sw_vers -productVersion)"
       MAC_OS_MINOR_VERSION="$(version_component "$MAC_OS_VERSION" 2)"
+      MAC_OS_MAJOR_VERSION="$(version_component "$MAC_OS_VERSION" 1)"
 
-      if [ "$MAC_OS_MINOR_VERSION" -lt 6 ]; then
+      if [ "$MAC_OS_MAJOR_VERSION" -eq 11 ]; then 
+        echo "Big Sur detected!" >&2
+      fi
+
+      elif [ "$MAC_OS_MINOR_VERSION" -lt 6 ]; then
         echo "Pow requires Mac OS X 10.6 or later." >&2
         exit 1
 
